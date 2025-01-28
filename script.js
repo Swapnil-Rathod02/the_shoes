2baa09acac1f223b26547997da9973f5c22d9fe9
Swapnil-Rathod02 <swapnilrathod1002@gmail.com>
2025-01-28 17:53:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", (e) => {
+  hamburger.classList.toggle("open-menu");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("open-menu", ".open-menu &");
+    }),
+  ],
+};
